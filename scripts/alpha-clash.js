e286a1f82e1c2c6a5acfdd96969180f05f0781ac
Sultana Razia <sultanarazia483@gmail.com>
2024-04-28 20:44:24 +0600

    Update Score

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -18,12 +18,37 @@ function handleKeyboardKeyUpEvent(event) {
     const expectedAlphabet = currentAlphabet.toLowerCase();
     console.log(playerPressed, expectedAlphabet);
 
-    //Check matched or not
+    //check matched or not
     if (playerPressed == expectedAlphabet) {
         console.log('You get a point');
+
+        //get the current score
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+
+        //increase the score
+        const newScore = currentScore + 1;
+
+        //show the update score
+        currentScoreElement.innerText = newScore;
+
+        //start a new round
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
     }
     else {
         console.log('You missed. You lost a life');
+        //get the current life number
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLife = parseInt(currentLifeElement.innerText);
+
+        //reduce the life count
+        const newLife = currentLife - 1;
+
+        //display the updated life count
+        currentLifeElement.innerText = newLife;
+
     }
 }
 
